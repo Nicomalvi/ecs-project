41b141939542f27df7b4ffab0f46960a5ef86f9d
Nicomalvi <nicomalvi@gmail.com>
2026-04-11 02:53:57 -0300

    gitignore

--- obj/Debug/net10.0/ecs-project.AssemblyInfo.cs
+++ obj/Debug/net10.0/ecs-project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ecs-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be84c64c9100a9a0ba68edfda3475284b7100d67")]
 [assembly: System.Reflection.AssemblyProductAttribute("ecs-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ecs-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
